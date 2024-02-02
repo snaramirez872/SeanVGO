c601e41131f5e881f7dbc5e8d71896d726e818a7
snaramirez872 <seanramirez818@gmail.com>
2024-02-01 19:35:14 -0500

    navbar styled

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./styles/NavBar.css";
 
 export default function NavBar() {
     return (
         <nav className="nav-bar">
-            <h1>SeanVGO</h1>
+            <div className="logo">
+                <h1>SeanVGO</h1>
+            </div>
             <div className="nav-links">
-                <Link to="/">
-                    Game List
-                </Link>
-                <Link to="/insert-game">
-                    Insert Game
-                </Link>
-                <Link to="/remove-game">
-                    Remove Game
-                </Link>
+                <ul className="links">
+                    <li>
+                        <Link to="/">
+                            <p>Game List</p>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/insert-game">
+                            <p>Insert Game</p>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/remove-game">
+                            <p>Remove Game</p>
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </nav>
     );
